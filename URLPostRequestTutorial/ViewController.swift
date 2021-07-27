bd181a2f20524e126eb03457306f920de0167c61
brainox <macmartins081@gmail.com>
2021-07-27 16:53:13 +0100

    setup project

--- URLPostRequestTutorial/ViewController.swift
+++ URLPostRequestTutorial/ViewController.swift
@@ -8,12 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+  
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func sendMessage(_ sender: Any) {
+        let alertController = UIAlertController(title: "New Message", message: "Enter a new Message", preferredStyle: .alert)
+    }
+    
+    
 }
 
